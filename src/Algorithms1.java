cefd5991cb56338d60b1ed837d0f99d597689e20
Andreas-Ravn <andr8343@edu.easj.dk>
2020-10-28 15:00:40 +0100

    Andreas Algorithms1/2/data

--- src/Algorithms1.java
+++ src/Algorithms1.java
@@ -9,13 +9,13 @@ import java.util.Random;
 
 public class Algorithms1 {
 
-    // TODO 1 - Gennemse Data klassen, og skriv gode kommentarer til det
+//done   // TODO 1 - Gennemse Data klassen, og skriv gode kommentarer til det
 
-    // TODO 2 - Skriv en randomBoyName() metode i Algorithms1, som returnerer et tilfældigt drengenavn fra Data klassens randomPigeNavne property OBS: randomPigeNavne skal forblive private!
+//done    // TODO 2 - Skriv en randomBoyName() metode i Algorithms1, som returnerer et tilfældigt drengenavn fra Data klassens randomDrengeNavne property OBS: randomDrengeNavne skal forblive private!
 
     // TODO 3 - Skriv en test til randomBoyName() metoden HINT: Se metoden exampleOfPredictableRandomNumber() for at se, hvordan du kan lave et tilfældigt nummer som er altid det samme (til test)
 
-    // TODO 4 - Skriv en randomGirlName() metode i Algorithms1, som returnerer et tilfældigt pigenavn fra Data klassen
+//done    // TODO 4 - Skriv en randomGirlName() metode i Algorithms1, som returnerer et tilfældigt pigenavn fra Data klassen
 
     // TODO 5 - Skriv en randomName() metode i Algorithms1, som laver et sammenlagt array som indeholder både piger og drenge, og returnerer det
 
@@ -27,13 +27,36 @@ public class Algorithms1 {
 
     public static void main(String[] args) {
         exampleOfPredictableRandomNumber();
+        randomBoyName();
+        randomGirlName();
+        randomBothName();
+
     }
 
     private static void exampleOfPredictableRandomNumber() {
         Random random = new Random();
-        random.setSeed(15L); // Hvis vi angiver et seed som et long number, vil random klassen altid generere det samme tal, i det her tilfælde 21
+        random.setSeed(15015L); // Hvis vi angiver et seed som et long number, vil random klassen altid generere det samme tal, i det her tilfælde 21
         System.out.print("Hvis vi bruger seed, vil random altid returnere de samme værdier, f.eks. giver denne 21 --> ");
         System.out.println(random.nextInt(45));
     }
+    public static void randomBoyName(){
+        Random rand = new Random();
+        int rng = rand.nextInt(44);
+        String[] names = new Data().getRandomDrengeNavne();
+        System.out.println(names[rng]);
 
+    }
+    public static void randomGirlName() {
+        Random rand = new Random();
+        int randomGenerator = rand.nextInt(44);
+        String[] names = new Data().getRandomPigeNavne();
+        System.out.println(names[randomGenerator]);
+    }
+    public static void randomBothName(){
+        Random rand = new Random();
+        int rngBoth = rand.nextInt(44);
+        String[] names = new Data().getRandomPigeNavne();
+        String[] names1 = new Data().getRandomDrengeNavne();
+        System.out.println(names[rngBoth] + " + " + names1[rngBoth]);
+    }
 }
